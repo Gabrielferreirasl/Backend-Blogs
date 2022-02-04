6b2aaa6aec029caed794f045cbcaa583b744b52d
Gabriel <gabrielferreiradsl@hotmail.com>
2022-02-04 17:26:43 -0300

    migrations criadas

---
+++ migrations/20220204200341-create-posts-category.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+      postId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        }
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
